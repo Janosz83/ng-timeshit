37eeb894391d4c9c11ab64f7a6e524cab0206630
Janosz83 <johnylaszlo1@gmail.com>
2023-05-15 16:32:26 +0200

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,48 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'timesheet';
+  title: string = 'timesheet';
+  mounts = {
+    january: {
+      days: 31,
+    },
+    february: {
+      days: 28,
+    },
+    march: {
+      days: 31,
+    },
+    april: {
+      days: 30,
+    },
+
+    may: {
+      days: 31,
+    },
+    june: {
+      days: 30,
+    },
+    july: {
+      days: 31,
+    },
+    august: {
+      days: 31,
+    },
+    september: {
+      days: 30,
+    },
+    october: {
+      days: 31,
+    },
+    november: {
+      days: 30,
+    },
+    december: {
+      days: 31,
+    },
+  };
+  constructor() {}
 }
