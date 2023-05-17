12007ef1f654bd7e02210f582b39066b391cf423
rgurgul <gurgul.robert@gmail.com>
2023-05-17 21:01:56 +0200

    grid

---
+++ src/app/components/grid/grid.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
+})
+export class GridComponent {
+
+@Input() data:any;
+
+}
+
