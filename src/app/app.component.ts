eb4304bef28911d295263b3c17057ffec9661137
Janosz83 <johnylaszlo1@gmail.com>
2023-05-18 18:11:16 +0200

    httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,44 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'timesheet';
-  mounts = {
-    january: {
-      days: 31,
-    },
-    february: {
-      days: 28,
-    },
-    march: {
-      days: 31,
-    },
-    april: {
-      days: 30,
-    },
+  data: any
 
-    may: {
-      days: 31,
-    },
-    june: {
-      days: 30,
-    },
-    july: {
-      days: 31,
-    },
-    august: {
-      days: 31,
-    },
-    september: {
-      days: 30,
-    },
-    october: {
-      days: 31,
-    },
-    november: {
-      days: 30,
-    },
-    december: {
-      days: 31,
-    },
-  };
-  constructor() {}
+  constructor(
+    private http:HttpClient
+  ) {
+    this.http.get('https://api.debugger.pl/items').subscribe((resp:any) =>{
+      this.data = resp.data
+
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { GridComponent } from './components/grid/grid.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
