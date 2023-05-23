2a4716fda7a07f07b3801e4a622ca2c2fe1dcbce
Janosz83 <johnylaszlo1@gmail.com>
2023-05-23 19:25:59 +0200

    style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'timesheet';
-  data: any
+  title: string = 'opel';
+  data: any;
+  color: string = 'lightblue';
 
-  constructor(
-    private http:HttpClient
-  ) {
-    this.http.get('https://api.debugger.pl/items').subscribe((resp:any) =>{
-      this.data = resp.data
+  constructor(private http: HttpClient) {
+    this.http.get('https://api.debugger.pl/items').subscribe((resp: any) => {
+      this.data = resp.data;
+    });
+  }
 
-    })
+  onClick(value: any) {
+    //  alert(value)
+    this.title = value;
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    console.log(randomColor);
+    this.color = '#' + randomColor;
   }
 }
