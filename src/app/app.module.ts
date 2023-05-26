1a6017af1e82867875b9d87134eb5d0e500314e1
Janosz83 <johnylaszlo1@gmail.com>
2023-05-26 11:15:50 +0200

    autobus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { AutobusComponent } from './components/autobus/autobus.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridComponent
+    GridComponent,
+    AutobusComponent
   ],
   imports: [
     BrowserModule,
